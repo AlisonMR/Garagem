0a95c473c56e154c26706697d244d586676dfb5e
Alison <alisonraffaelly@gmail.com>
2023-08-25 09:32:58 -0300

    init concertado de vez

---
+++ garagem/serializers/__init__.py
@@ -0,0 +1,6 @@
+from .Acessorio import AcessorioSerializer
+from .Categoria import CategoriaSerializer
+from .Cor import CorSerializer
+from .Marca import MarcaSerializer
+from .Modelo import ModeloSerializer
+from .Veiculo import VeiculoSerializer, VeiculoListSerializer, VeiculoDetailSerializer
---
+++ garagem/views/__init__.py
@@ -0,0 +1,6 @@
+from .Acessorio import AcessorioViewSet
+from .Categoria import CategoriaViewSet
+from .Cor import CorViewSet
+from .Marca import MarcaViewSet
+from .Modelo import ModeloViewSet
+from .Veiculo import VeiculoViewSet
