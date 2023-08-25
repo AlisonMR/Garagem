1c5dd16a0975c8a47a8b85c83ae4c85be4f77a7e
Alison <alisonraffaelly@gmail.com>
2023-08-25 09:19:04 -0300

    organzação de pastas

---
+++ garagem/serializers/Veiculo.py
@@ -0,0 +1,9 @@
+from rest_framework.serializers import ModelSerializer
+
+from garagem.models import Veiculo
+
+
+class VeiculoSerializer(ModelSerializer):
+    class Meta:
+        model = Veiculo
+        fields = "__all__"
